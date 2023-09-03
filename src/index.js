7940fc40a02b141cd284788601bcd63178255336
DialecticalLaw <d.shmuratkin@mail.ru>
2023-09-03 12:43:40 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = [];
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '|') {
+      arr.push(str[i]);
+    }
+    if (str[i] === ')' || str[i] === ']' || str[i] === '}' || str[i] === '|') {
+      if (arr[arr.length - 1] === '(' && str[i] === ')') {
+        arr.pop();
+      } else if (arr[arr.length - 1] === '[' && str[i] === ']') {
+        arr.pop();
+      } else if (arr[arr.length - 1] === '{' && str[i] === '}') {
+        arr.pop();
+      } else if (arr[arr.length - 1] === '|' && str[i] === '|') {
+        arr.pop();
+      } else {
+        arr.push(str[i]);
+      }
+    }
+  }
+  if (arr.length) {
+    return false;
+  }
+  return true;
 }
